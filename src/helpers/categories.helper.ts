3ba1312404a6a8c95fdaa3f08ed8c0f58b48cf39
Eric Moraes <eric.guedes.sm@gmail.com>
2023-03-23 13:02:29 -0300

    fix category helper

--- src/helpers/categories.helper.ts
+++ src/helpers/categories.helper.ts
@@ -1,18 +1,16 @@
 import { formatedCategory, rawCategory } from '../types/categories';
 
-export const formatCategoriesObject = async (
+export const formatCategoriesObject = (
 	categories: rawCategory[]
-): Promise<formatedCategory[]> => {
-	const formatedCategories = await Promise.all(
-		categories.map(async (category, index) => {
-			return {
-				id: category.id,
-				description: category.description,
-				name: category.name,
-				parentId: category.parent,
-			};
-		})
-	);
+): formatedCategory[] => {
+	const formatedCategories = categories.map((category, index) => {
+		return {
+			id: category.id,
+			description: category.description,
+			name: category.name,
+			parentId: category.parent,
+		};
+	});
 
 	return formatedCategories;
 };
